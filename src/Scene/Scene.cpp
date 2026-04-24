581d130e32d9fe3df2d0158dc5e924c9d625dbd1
Ayrton LUDGER-CROSNIER <ayrton.ludger-crosnier@epitech.eu>
2026-04-24 15:48:53 +0200

    feat: add Scene structur with Iprimitive vector only

--- src/Scene/Scene.cpp
+++ src/Scene/Scene.cpp
@@ -9,8 +9,42 @@
 
 namespace RayTracer
 {
-    Scene::Scene(std::vector<std::unique_ptr<IPrimitive>> primitives):_primitives(std::move(primitives))
+    Scene::Scene(std::vector<std::unique_ptr<IPrimitive>> primitives):
+        _camera(Camera(Ameth::Vec3D(0.0, 200, -350), Ameth::Quaternion::identity(), 2.0 * std::atan(0.5), 1920, 1080)),
+        _primitives(std::move(primitives))
     {
+        
+    }
 
+    bool Scene::rayHitPrimitive(Camera::Ray &ray)
+    {
+        for (auto &&primitive : _primitives) {
+            if (primitive->hits(ray)) {
+                _hitColor = Ameth::Color(1.0, 0.0, 0.0);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    std::vector<Ameth::Color> Scene::getScreen()
+    {
+        constexpr unsigned WIDTH = 1920;
+        constexpr unsigned HEIGHT = 1080;
+    
+        for (unsigned x = 0; x < WIDTH; ++x) {
+            for (unsigned y = 0; y < HEIGHT; ++y) {
+                double u = (static_cast<double>(x) + 0.5) / static_cast<double>(WIDTH);
+                double v = (static_cast<double>(y) + 0.5) / static_cast<double>(HEIGHT);
+                Camera::Ray r = _camera.ray(u, v);
+                std::size_t i = y * WIDTH + x;
+                if (rayHitPrimitive(r)) {
+                    _camera.getHDRImage()[i] = _hitColor;
+                } else {
+                    _camera.getHDRImage()[i] = Ameth::Color(0.0, 0.0, 1.0);
+                }
+            }
+        }
+        return _camera.getHDRImage();
     }
 }
--- src/Scene/Scene.hpp
+++ src/Scene/Scene.hpp
@@ -7,14 +7,30 @@
 
 #pragma once
 #include "plugins/IPrimitive.hpp"
+#include "plugins/ILight.hpp"
+#include "plugins/IMaterial.hpp"
+#include "Camera/Camera.hpp"
 #include <memory>
 namespace RayTracer
 {
     class Scene {
         public:
-            Scene(std::vector<std::unique_ptr<IPrimitive>> primitives);
+            Scene(/* Camera &cam, */
+                std::vector<std::unique_ptr<IPrimitive>> primitives
+                // std::vector<std::unique_ptr<ILight>> &Lights,
+                // std::vector<std::unique_ptr<IMaterial>> &materials
+                );
+
+            std::vector<Ameth::Color> getScreen();
 
         private:
+            Camera _camera;
             std::vector<std::unique_ptr<IPrimitive>> _primitives;
+            std::vector<std::unique_ptr<ILight>> _lights;
+            std::vector<std::unique_ptr<IMaterial>> _materials;
+
+            Ameth::Color _hitColor;
+
+            bool rayHitPrimitive(Camera::Ray &ray);
     };
 }
