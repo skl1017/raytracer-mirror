faaa3cf7a23dfd39de2f968a803f4b4a389c72be
Ayrton LUDGER-CROSNIER <ayrton.ludger-crosnier@epitech.eu>
2026-04-27 10:24:15 +0200

    feat: add directional light

--- plugins/Lights/Directional/DirectionalLight.cpp
+++ plugins/Lights/Directional/DirectionalLight.cpp
@@ -7,10 +7,13 @@
 
 #include "DirectionalLight.hpp"
 
-DirectionalLight::DirectionalLight(/* args */)
+Ameth::Color DirectionalLight::getIllumination(Ray::HitRecord &hitRecord)
 {
-}
+    Ameth::Color illumination = _LightColor;
+    Ameth::Vec3D incidentLight = Ameth::Vec3D(0, 0, 0) - _direction;
+    double ambient = 0.1;
+    double angle = hitRecord.normal.dot(incidentLight.normalized());
+    double intensity = ambient + std::max(0.0, angle);
 
-DirectionalLight::~DirectionalLight()
-{
+    return illumination * intensity;
 }
--- plugins/Lights/Directional/DirectionalLight.hpp
+++ plugins/Lights/Directional/DirectionalLight.hpp
@@ -11,6 +11,11 @@
 class DirectionalLight : public ILight
 {
 public:
-    DirectionalLight(/* args */);
-    ~DirectionalLight();
+    DirectionalLight(Ameth::Vec3D direction, Ameth::Color lightColor) :
+        _direction(direction), _LightColor(lightColor) {};
+    ~DirectionalLight() = default;
+    Ameth::Color getIllumination(Ray::HitRecord &hitRecord) override;
+protected:
+    Ameth::Vec3D _direction;
+    Ameth::Color _LightColor;
 };
