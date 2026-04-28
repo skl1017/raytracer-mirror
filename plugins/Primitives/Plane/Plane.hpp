fdd65c4b4aa4b6adff1b2d3a99132fe39495e8fa
Loan RIYANTO <loan.riyanto@epitech.eu>
2026-04-28 15:51:38 +0200

    fix: adapted merged changes

--- plugins/Primitives/Plane/Plane.hpp
+++ plugins/Primitives/Plane/Plane.hpp
@@ -6,19 +6,23 @@
 */
 
 #pragma once
-#include "plugins/IPrimitive.hpp"
+#include "plugins/APrimitive.hpp"
 #include "PluginFactory/PluginFactory.hpp"
 #include "Math/Ameth.hpp"
+#include "Camera/Camera.hpp"
 namespace RayTracer
 {
-    class Plane : public IPrimitive
+    class Plane : public APrimitive
     {
         public:
             Plane(char, double, const Ameth::Color &);
 
             std::string getName() const{};
-            Ameth::Vec3D pointAt(double u, double v){};
-            bool hits(Camera::Ray const &ray){};
+            Ameth::Vec3D pointAt(double u, double v) const override{}
+
+        protected:
+            void fillHitRecord(Ray const &ray, double t, Ray::HitRecord &rec) const override{}
+            std::optional<std::pair<double, double>> lineTValues(Ameth::Vec3D const &origin, Ameth::Vec3D const &dir) const{}
 
 
 
