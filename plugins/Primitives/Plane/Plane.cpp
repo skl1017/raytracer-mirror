2f172172c6e8f8cd97308651aaf0e21735a801a9
rayan <rayan.ouerdane@epitech.eu>
2026-05-12 17:57:17 +0200

    feat: init plane

--- plugins/Primitives/Plane/Plane.cpp
+++ plugins/Primitives/Plane/Plane.cpp
@@ -10,22 +10,15 @@
 namespace RayTracer
 {
     Plane::Plane(char axis, double position, const Ameth::Color& color):_axis(axis),_position(position),_color(color){}
-
-    extern "C"
-    {
-        void registerPlugin(PluginFactory &factory){
-            PluginFactory::iPrimitiveCreateFunction f = [](const RayTracer::PluginFactory::primitivePayload &p)
-            {
-                auto planePayload = std::get<PluginFactory::plane_payload_t>(p);
-                return std::make_unique<Plane>(planePayload.axis, planePayload.position, planePayload.color);
-            };
-            factory.add("plane", f);
-            return;
-        }
-
-        PLUGIN getLibType()
-        {
-            return PRIMITIVE;
-        }
-    }
 }
+
+extern "C" void registerPlugin(RayTracer::PluginFactory &factory)
+{
+    RayTracer::PluginFactory::iPrimitiveCreateFunction const f
+        = [](RayTracer::PluginFactory::primitivePayload const &p) -> std::unique_ptr<IPrimitive> {
+        auto const planePayload = std::get<RayTracer::PluginFactory::plane_payload_t>(p);
+        return std::make_unique<RayTracer::Plane>(planePayload.axis, planePayload.position, planePayload.color);
+    };
+    factory.add("plane", f);
+}
+
